1ed22f603e0d062747c3cac0dfa5a2e35a1383f9
Mohammed Mothaffar <109213119+MohammedMothaffar@users.noreply.github.com>
2023-01-19 14:25:07 +0200

    amazon-clone

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className='home'>
+<div classeName='home__container'>
+<img className="home__image" src="prime.jpg" alt="" />
+<div className="home__row">
+<Product/>
+<Product/>
+
+</div>
+<div className="home__row">
+{/*Product */}
+{/*Product */}
+{/*Product */}
+</div>
+
+<div className="home__row">
+{/*Product */}
+</div>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ3Bg8IGFXn9NZttJbbcaarUDnhp5qRsg",
+    authDomain: "challenge-d954c.firebaseapp.com",
+    projectId: "challenge-d954c",
+    storageBucket: "challenge-d954c.appspot.com",
+    messagingSenderId: "605752638940",
+    appId: "1:605752638940:web:f369fbfefa7caa602ae319",
+    measurementId: "G-89RTSHX5LW"
+  };
